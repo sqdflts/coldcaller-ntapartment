adfd3d420b2b1976ba505131196d71aa50ca33c5
tcpstrm <132112768+sqdflts@users.noreply.github.com>
2025-07-14 23:30:32 +0200

    Update ntapartment.h

--- coldcaller/ntapartment/ntapartment.h
+++ coldcaller/ntapartment/ntapartment.h
@@ -6,4 +6,3 @@ extern int ntacc_called;
 // functions lol
 
 extern "C" void* ntacc_createthreadex(PHANDLE ThreadHandle, ACCESS_MASK DesiredAccess, PVOID ObjectAttributes, HANDLE ProcessHandle, PVOID StartRoutine, PVOID Argument, ULONG CreateFlags, SIZE_T ZeroBits, SIZE_T StackSize, SIZE_T MaximumStackSize, PVOID AttributeList, BYTE syscall_id, BOOL debug);
-void ntacc_lickbigcock(); //homo
